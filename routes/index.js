f4eb5951ee5b9789b49908eb3e408850ebcd0954
benjamineruvieru <benjamineruvieru@gmail.com>
2023-04-03 11:26:43 +0100

    task

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ const UsersController = require("../controllers/UsersController");
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
 router.post("/users", UsersController.postNew);
+router.get("/connect", AppController.getConnect);
+router.get("/disconnect", AppController.getDisconnect);
+router.post("/users/me", UsersController.getMe);
 
 module.exports = router;
