7f355ceb1a79d30feac8e4a0a681cb1d285e9a23
benjamineruvieru <benjamineruvieru@gmail.com>
2023-04-03 09:13:11 +0100

    Controller

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+const { isRedisAlive, isDBAlive, countUsers, countFiles } = require("../utils");
+const dbClient = require("../utils/db");
+const redisClient = require("../utils/redis");
+
+const AppController = {
+  async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = await dbClient.isAlive();
+
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  },
+
+  async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    res.status(200).json({ users, files });
+  },
+};
+
+module.exports = AppController;
