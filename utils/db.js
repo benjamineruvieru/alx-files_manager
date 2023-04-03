305d73ec1a59aae74c11f66d081bb81ff006adb3
benjamineruvieru <benjamineruvieru@gmail.com>
2023-04-03 08:57:03 +0100

    db

---
+++ utils/db.js
@@ -0,0 +1,51 @@
+const { MongoClient } = require("mongodb");
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || "localhost";
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || "files_manager";
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri);
+  }
+
+  async isAlive() {
+    try {
+      await this.client.connect();
+      return true;
+    } catch (error) {
+      return false;
+    } finally {
+      await this.client.close();
+    }
+  }
+
+  async nbUsers() {
+    try {
+      await this.client.connect();
+      const users = this.client.db().collection("users");
+      const count = await users.countDocuments();
+      return count;
+    } catch (error) {
+      return -1;
+    } finally {
+      await this.client.close();
+    }
+  }
+
+  async nbFiles() {
+    try {
+      await this.client.connect();
+      const files = this.client.db().collection("files");
+      const count = await files.countDocuments();
+      return count;
+    } catch (error) {
+      return -1;
+    } finally {
+      await this.client.close();
+    }
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
