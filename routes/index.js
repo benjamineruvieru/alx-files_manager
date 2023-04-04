e464ccd8efb0e30d221c27b88c766731cdd3d7ab
benjamineruvieru <benjamineruvieru@gmail.com>
2023-04-04 21:58:57 +0100

    files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const AppController = require("../controllers/AppController");
 const UsersController = require("../controllers/UsersController");
+const FilesController = require("../controllers/FilesController");
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
@@ -10,5 +11,6 @@ router.post("/users", UsersController.postNew);
 router.get("/connect", AppController.getConnect);
 router.get("/disconnect", AppController.getDisconnect);
 router.post("/users/me", UsersController.getMe);
+router.post("files", FilesController.postUpload);
 
 module.exports = router;
